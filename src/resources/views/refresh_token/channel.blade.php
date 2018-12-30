bdabbed7640fd3814fe74dac0ef7e9c489702ac1
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-12-30 14:53:10 +0100

    Disable buttons if slack/discord is not setup

--- src/resources/views/refresh_token/channel.blade.php
+++ src/resources/views/refresh_token/channel.blade.php
@@ -1,6 +1,6 @@
 @inject('RefreshTokenController', 'Herpaderpaldent\Seat\SeatNotifications\Http\Controllers\Notifications\RefreshTokenController')
 
-@if(! (auth()->user()->has('seatnotifications.refresh_token', false) && auth()->user()->has('seatnotifications.configuration', false)))
+@if(! (auth()->user()->has('seatnotifications.refresh_token', false) && auth()->user()->has('seatnotifications.configuration', false)) || is_null(setting('herpaderp.seatnotifications.discord.credentials.bot_token', true)))
     <a href="" type="button" class="btn btn-app disabled">
       <i class="fa fa-bullhorn"></i>Discord
     </a>
@@ -15,7 +15,7 @@
   </a>
 @endif
 
-@if(! (auth()->user()->has('seatnotifications.refresh_token', false) && auth()->user()->has('seatnotifications.configuration', false)))
+@if(! (auth()->user()->has('seatnotifications.refresh_token', false) && auth()->user()->has('seatnotifications.configuration', false)) || is_null(setting('herpaderp.seatnotifications.slack.credentials.token', true)))
   <a href="" type="button" class="btn btn-app disabled">
     <i class="fa fa-slack"></i>Slack
   </a>

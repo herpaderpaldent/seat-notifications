7a79eafaf0abdd285dbcd2fdcbc5cdebbc6b0948
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-01-31 22:26:35 +0100

    Working discord messages with alliance ticker

--- src/Notifications/KillMailNotification.php
+++ src/Notifications/KillMailNotification.php
@@ -13,15 +13,10 @@ use Herpaderpaldent\Seat\SeatNotifications\Channels\Discord\DiscordChannel;
 use Herpaderpaldent\Seat\SeatNotifications\Channels\Discord\DiscordMessage;
 use Herpaderpaldent\Seat\SeatNotifications\Channels\Slack\SlackChannel;
 use Herpaderpaldent\Seat\SeatNotifications\Channels\Slack\SlackMessage;
-use Illuminate\Http\Request;
-use Seat\Eveapi\Models\Killmails\CorporationKillmail;
-use Seat\Eveapi\Models\Killmails\KillmailAttacker;
 use Seat\Eveapi\Models\Killmails\KillmailDetail;
-use Seat\Eveapi\Models\Killmails\KillmailVictim;
 use Seat\Eveapi\Models\Killmails\KillmailVictimItem;
 use Seat\Eveapi\Models\Market\Price;
 use Seat\Eveapi\Models\Sde\InvType;
-use Seat\Eveapi\Models\Universe\UniverseName;
 
 class KillMailNotification extends BaseNotification
 {
@@ -65,11 +60,6 @@ class KillMailNotification extends BaseNotification
     public function via($notifiable)
     {
 
-        $this->is_loss = $notifiable
-            ->notifications
-            ->firstwhere('name','kill_mail')
-            ->hasAffiliation('corp', $this->killmail_detail->victims->corporation_id);
-
         switch ($notifiable->notification_channel) {
             case 'discord':
                 $this->tags = [
@@ -85,7 +75,6 @@ class KillMailNotification extends BaseNotification
                     'slack',
                     $notifiable->type === 'private' ? $notifiable->recipient() : 'channel',
                 ];
-
                 return [SlackChannel::class];
                 break;
             default:
@@ -97,27 +86,17 @@ class KillMailNotification extends BaseNotification
     public function toDiscord($notifiable)
     {
 
-        // if it's a kill
-        $title = sprintf('%s just killed %s',
-            $this->getAttacker(),
-            $this->getVictim($this->killmail_detail->killmail_id)
-        );
-
-        /*if($this->is_loss)
-            $title = sprintf('')*/
-
         return (new DiscordMessage)
-            ->embed(function ($embed) use ($title) {
+            ->embed(function ($embed) use ($notifiable){
 
-                $embed->title($title)
+                $embed->title($this->getNotificationString('discord'))
                     ->thumbnail($this->image)
-                    ->color($this->is_loss ? '14502713' : '42586')
+                    ->color($this->is_loss($notifiable) ? '14502713' : '42586')
                     ->field('Value', $this->getValue($this->killmail_detail->killmail_id))
+                    ->field('Involved Pilots', $this->getNumberOfAttackers(), true)
                     ->field('System', $this->getSystem(), true)
                     ->field('Link', $this->zKillBoardToLink('kill', $this->killmail_detail->killmail_id), true)
                     ->footer('zKillboard ' . $this->killmail_detail->killmail_time, 'https://zkillboard.com/img/wreck.png');
-                    //->field('Corporation', $this->corporation, true)
-                    //->field('Main Character', $this->main_character, false);
             });
     }
 
@@ -126,7 +105,7 @@ class KillMailNotification extends BaseNotification
      *
      * @return \Herpaderpaldent\Seat\SeatNotifications\Channels\Slack\SlackMessage
      */
-    /*public function toSlack($notifiable)
+    public function toSlack($notifiable)
     {
         return (new SlackMessage)
             ->warning()
@@ -139,7 +118,7 @@ class KillMailNotification extends BaseNotification
                     ])
                     ->thumb($this->image);
             });
-    }*/
+    }
 
     /**
      * Build a link to zKillboard using Slack message formatting.
@@ -159,28 +138,82 @@ class KillMailNotification extends BaseNotification
         return sprintf('https://zkillboard.com/%s/%d/', $type, $id);
     }
 
-    private function getAttacker() :string
+    private function getNotificationString(string $channel) : string
+    {
+        return sprintf('%s just killed %s %s',
+            $this->getAttacker($channel),
+            $this->getVictim($channel),
+            $this->getNumberOfAttackers($channel) === 1 ? 'solo.' : ''
+        );
+    }
+
+    private function getAttacker($channel) :string
     {
         $killmail_attacker = $this->killmail_detail
             ->attackers
             ->where('final_blow', 1)
             ->first();
 
-        return sprintf('**%s** (%s) flying a **%s**',
-            //optional(UniverseName::find($killmail_attacker->character_id))->name,
-            $this->resolveID($killmail_attacker->character_id),
-            $this->resolveID($killmail_attacker->corporation_id),
-            optional(InvType::find($killmail_attacker->ship_type_id))->typeName);
+        if($channel === 'discord')
+            return $this->getDiscordKMStringPartial(
+                $killmail_attacker->character_id,
+                $killmail_attacker->corporation_id,
+                $killmail_attacker->ship_type_id,
+                $killmail_attacker->alliance_id
+            );
+
+        return '';
     }
 
-    private function getVictim(int $killmail_id) :string
+    /**
+     * @param int $killmail_id
+     *
+     * @return string
+     */
+    private function getVictim($channel) :string
     {
-        $killmail_victim = KillmailVictim::find($killmail_id);
+        $killmail_victim = $this->killmail_detail->victims;
 
-        return sprintf('**%s** (%s) flying a **%s**',
-            $this->resolveID($killmail_victim->character_id),
-            $this->resolveID($killmail_victim->corporation_id),
-            optional(InvType::find($killmail_victim->ship_type_id))->typeName);
+        if($channel === 'discord')
+            return $this->getDiscordKMStringPartial(
+                $killmail_victim->character_id,
+                $killmail_victim->corporation_id,
+                $killmail_victim->ship_type_id,
+                $killmail_victim->alliance_id
+            );
+
+        return '';
+    }
+
+    private function getNumberOfAttackers() : int
+    {
+
+        return $this->killmail_detail->attackers->count();
+    }
+
+    private function getDiscordKMStringPartial($character_id, $corporation_id, $ship_type_id, $alliance_id) : string
+    {
+        $character = is_null($character_id) ? null : $this->resolveID($character_id);
+        $corporation = $this->resolveID($corporation_id);
+        $alliance = is_null($alliance_id) ? null :  strtoupper(' ' . $this->resolveID($alliance_id, true));
+        $ship_type = optional(InvType::find($ship_type_id))->typeName;
+
+        if(is_null($character_id))
+            return sprintf('**%s** (%s%s)',
+                $ship_type,
+                $corporation,
+                $alliance
+            );
+
+        if (!is_null($character_id))
+            return sprintf('**%s** (%s%s) flying a **%s**',
+                $character,
+                $corporation,
+                $alliance,
+                $ship_type
+            );
+
+        return '';
     }
 
     private function getValue(int $killmail_id) :string
@@ -196,6 +229,14 @@ class KillMailNotification extends BaseNotification
         return number($value) . ' ISK';
     }
 
+    private function is_loss($notifiable) : bool
+    {
+        return $notifiable
+            ->notifications
+            ->firstwhere('name','kill_mail')
+            ->hasAffiliation('corp', $this->killmail_detail->victims->corporation_id);
+    }
+
     private function getSystem() : string
     {
         $solar_system = $this->killmail_detail->solar_system;
@@ -207,21 +248,42 @@ class KillMailNotification extends BaseNotification
         );
     }
 
-    private function resolveID($id)
+    private function resolveID($id, $is_alliance = false)
     {
         $cached_entry = cache('name_id:' . $id);
 
         if(! is_null($cached_entry))
             return $cached_entry;
 
+        if($is_alliance)
+            return $this->getAllianceTicker($id);
+
         // Resolve the Esi client library from the IoC
         $eseye = app('esi-client')->get();
         $eseye->setBody([$id]);
         $names = $eseye->invoke('post', '/universe/names/');
 
+        var_dump($names);
+
         $name = collect($names)->first()->name;
 
         return $name;
+    }
+
+    private function getAllianceTicker($id)
+    {
+        $cached_entry = cache('alliance_ticker:' . $id);
+
+        if(! is_null($cached_entry))
+            return $cached_entry;
+
+        // Resolve the Esi client library from the IoC
+        $eseye = app('esi-client')->get();
+        $ticker = $eseye->invoke('get', '/alliances/' . $id)->ticker;
+
+        cache(['alliance_ticker:' . $id => $ticker], carbon()->addCentury());
+
+        return $ticker;
     }
 
 

5ef5612cba7df792570af03dbb9f247074371376
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-03-02 22:06:20 +0000

    Apply fixes from StyleCI

--- src/Exceptions/UnknownDriverException.php
+++ src/Exceptions/UnknownDriverException.php
@@ -3,22 +3,19 @@
  * Created by PhpStorm.
  * User: felix
  * Date: 02.03.2019
- * Time: 22:40
+ * Time: 22:40.
  */
 
 namespace Herpaderpaldent\Seat\SeatNotifications\Exceptions;
 
-
 use Exception;
-use Throwable;
 
 class UnknownDriverException extends Exception
 {
-    public function __construct(string $provider, string $message = "", int $code = 0, \Throwable $previous = null)
+    public function __construct(string $provider, string $message = '', int $code = 0, \Throwable $previous = null)
     {
         $message = sprintf('An unknown driver implemention of %s was requested ', $provider);
 
         parent::__construct($message, $code, $previous);
     }
-
 }
